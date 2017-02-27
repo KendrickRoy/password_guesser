cfe1c3e9d16414bc6c4a6221b71a895749f431c5
Kendrick Roy <roykendrick934@gmail.com>
2017-02-27 22:55:10 +0000

    password guesser

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    $("button").click(function(){
+        var password=$("input").val();
+        alert(password)
+        if(password === computer science){
+            alert("hi")
+        }
+    });
 })
