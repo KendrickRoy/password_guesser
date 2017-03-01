ca1f62a5611d0e9781fa7388ce9972409c0ee4d8
Kendrick Roy <roykendrick934@gmail.com>
2017-03-01 22:28:57 +0000

    password guesser

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# password_guesser
+This project pops up a certain piece of text when you type up the correct password
+in the input bar and click the submit button.
+
+This project functions on a bunch of different function like click, hide and show. This
+is also functioned on an id tag i used to identify the message that pops up when you click
+the submit button, if you type in the correct password.
+
+Things that i would add to this project if i had more time would be a bigger input bar,
+a better background, and a more dramatic effect when you get the correct password.
--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 $(document).ready(function(){
+    var guesses = 3
+     $("#result").hide();
+     $("#other_result").hide();
+     $("#alternate_result").hide();
     $("button").click(function(){
         var password=$("input").val();
-        alert(password)
-        if(password === computer science){
-            alert("hi")
+        if(guesses === 0){
+            $("#alternate_result").show();
+            $("#other_result").hide();
+        }
+         else if(password === "computer science"){
+            $("#result").show();
+            $("#other_result").hide();
+        }
+        else{
+            $("#other_result").show();
+            $("#result").hide();
+            guesses = guesses - 1
+            
         }
     });
 })
